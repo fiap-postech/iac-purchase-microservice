2fbb8dce1a70095730f9f2c831ee1dbf0e28f066
Eberson Oliveira <eberson.olivera@gmail.com>
2024-01-25 18:27:36 -0300

    feat: added dlq sqs

--- sqs.tf
+++ sqs.tf
@@ -6,6 +6,30 @@ resource "aws_sqs_queue" "payment_done_queue" {
   receive_wait_time_seconds  = local.sqs.receive_wait_time_seconds
   visibility_timeout_seconds = local.sqs.visibility_timeout_seconds
   sqs_managed_sse_enabled    = local.sqs.sqs_managed_sse_enabled
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.payment_done_queue_dlq.arn,
+    maxReceiveCount     = 3
+  })
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = ["${aws_sqs_queue.payment_done_queue_dlq.arn}"]
+  })
+
+  depends_on = [
+    aws_sqs_queue.payment_done_queue_dlq
+  ]
+}
+
+resource "aws_sqs_queue" "payment_done_queue_dlq" {
+  name                       = "${local.sqs.name}-dlq"
+  delay_seconds              = local.sqs.delay_seconds
+  max_message_size           = local.sqs.max_message_size
+  message_retention_seconds  = local.sqs.message_retention_seconds
+  receive_wait_time_seconds  = local.sqs.receive_wait_time_seconds
+  visibility_timeout_seconds = local.sqs.visibility_timeout_seconds
+  sqs_managed_sse_enabled    = local.sqs.sqs_managed_sse_enabled
 }
 
 resource "aws_sns_topic_subscription" "get_payment_done_events" {
